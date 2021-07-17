3df5829a43de68182a30b3fe88786435b0bf9073
J. Nick Koston <nick@koston.org>
2021-07-17 08:33:27 -1000

    Adjust login for latest ASair changes

--- delete_mobile_phones.py
+++ delete_mobile_phones.py
@@ -6,14 +6,17 @@ import argparse
 import pprint
 
 from nexia.home import NexiaHome
+from nexia.const import BRAND_NEXIA
 
 parser = argparse.ArgumentParser()
+parser.add_argument("--brand", type=str, help="Brand (nexia or asair or trane).")
 parser.add_argument("--username", type=str, help="Your Nexia username/email address.")
 parser.add_argument("--password", type=str, help="Your Nexia password.")
 args = parser.parse_args()
+brand = args.brand or BRAND_NEXIA
 
 if args.username and args.password:
-    nexia_home = NexiaHome(username=args.username, password=args.password)
+    nexia_home = NexiaHome(username=args.username, password=args.password, brand=brand)
 else:
     parser.print_help()
     exit()
