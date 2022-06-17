37eab6532b53af39f3999c6cc849872967f6da58
J. Nick Koston <nick@koston.org>
2022-06-16 19:28:38 -1000

    Bump version: 1.0.1 → 1.0.2

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/bdraco/nexia",
-    version="1.0.1",
+    version="1.0.2",
     zip_safe=False,
 )
