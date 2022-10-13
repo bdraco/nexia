6179e63aea651eb497d0b4c59513fba7d712ffb8
J. Nick Koston <nick@koston.org>
2022-10-13 12:14:34 -1000

    fix: tests

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ test_requirements = [
 setup(
     author="J. Nick Koston",
     author_email="nick@koston.org",
-    python_requires=">=3.8",
+    python_requires=">=3.9",
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
