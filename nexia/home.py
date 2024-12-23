f19c695535a1c4416640de90dcbebad9765b9766
jrhillery <john.hillery2@gmail.com>
2024-12-23 15:12:57 -0500

    Changes to obtain sensor details in a Nexia Thermostat environment

--- nexia/home.py
+++ nexia/home.py
@@ -8,6 +8,7 @@ import logging
 from typing import TYPE_CHECKING, Any
 
 import orjson
+from yarl import URL
 
 from .automation import NexiaAutomation
 from .const import (
@@ -95,6 +96,7 @@ class NexiaHome:
         self._uuid = None
         self.session = session
         self.loop = asyncio.get_running_loop()
+        self.log_response = False
 
     @property
     def API_MOBILE_PHONE_URL(self) -> str:  # pylint: disable=invalid-name
@@ -134,6 +136,20 @@ class NexiaHome:
         """The mobile url for the service."""
         return MOBILE_URL_TEMPLATE.format(self.root_url)
 
+    def resolve_url(self, raw_path: str) -> str:
+        """Determine the url of the specified raw path on the root host
+        :param raw_path: url path with possibly incorrect host
+        :return: url resolved on the root host
+        """
+        root_url = URL(self.root_url)
+        root_host = root_url.host
+        url = URL(raw_path)
+
+        if root_host:
+            url = url.with_scheme(root_url.scheme).with_host(root_host)
+
+        return str(url)
+
     def _api_key_headers(self) -> dict[str, str]:
         headers = {
             "X-AppVersion": APP_VERSION,
@@ -144,6 +160,24 @@ class NexiaHome:
         if self.api_key:
             headers["X-ApiKey"] = str(self.api_key)
         return headers
+
+    async def _debug_log_resp(self, response: aiohttp.ClientResponse) -> None:
+        if self.log_response:
+            _LOGGER.debug(
+                "%s: Response from url %s: status: %s:\n%s",
+                response.method,
+                response.url,
+                response.status,
+                (await response.text()).strip(),
+            )
+        else:
+            _LOGGER.debug(
+                "%s: Response from url %s: status: %s: %s",
+                response.method,
+                response.url,
+                response.status,
+                response.content,
+            )
 
     async def post_url(self, request_url: str, payload: dict) -> aiohttp.ClientResponse:
         """Posts data to the session from the url and payload
@@ -167,7 +201,7 @@ class NexiaHome:
             max_redirects=MAX_REDIRECTS,
         )
 
-        _LOGGER.debug("POST: Response from url %s: %s", request_url, response.content)
+        await self._debug_log_resp(response)
         if response.status == 302:
             # assuming its redirecting to login
             _LOGGER.debug(
@@ -202,11 +236,7 @@ class NexiaHome:
             headers=headers,
             max_redirects=MAX_REDIRECTS,
         )
-        _LOGGER.debug(
-            "GET: RESPONSE %s: response.status %s",
-            request_url,
-            response.status,
-        )
+        await self._debug_log_resp(response)
 
         if response.status == 302:
             _LOGGER.debug(
---
+++ nexia/sensor.py
@@ -0,0 +1,36 @@
+"""Nexia Thermostat Sensor."""
+
+import dataclasses
+from typing import Optional, Type, TypeVar
+
+
+@dataclasses.dataclass
+class NexiaSensor:
+    """Data object representing details of a nexia sensor"""
+
+    id: int
+    name: str
+    type: str
+    serial_number: str
+    weight: float
+    temperature: int
+    temperature_valid: bool
+    humidity: int
+    humidity_valid: bool
+    has_online: bool
+    connected: Optional[bool]
+    has_battery: bool
+    battery_level: Optional[int]
+    battery_low: Optional[bool]
+    battery_valid: Optional[bool]
+
+    T = TypeVar("T", bound="NexiaSensor")
+
+    @classmethod
+    def from_json(cls: Type[T], sensor_json) -> T:
+        """Factory method for json data.
+        :param sensor_json: json dict with some or all of our fields
+        :return: a NexiaSensor instance
+        """
+        return cls(*[sensor_json.get(fld.name)
+                     for fld in dataclasses.fields(NexiaSensor)])
